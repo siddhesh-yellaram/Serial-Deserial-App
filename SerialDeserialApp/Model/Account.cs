3fc3c6c45a4f0a3a2f3c429384a2384ebd0920db
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 01:56:43 +0530

    Add files via upload

---
+++ SerialDeserialApp/Model/Account.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountApp.Model
+{
+    [Serializable]
+    class Account
+    {
+        private string _name;
+        private int _accNo;
+        private int _accBal;
+
+        public Account(string name, int accNo) : this("sid", 102, 1000)
+        { }
+
+        public Account(string name, int accNo, int accBal)
+        {
+            _name = name;
+            _accNo = accNo;
+            _accBal = accBal;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = "abc";
+            }
+        }
+
+        public int AccNo
+        {
+            get
+            {
+                return _accNo;
+            }
+            set
+            {
+                if (value > 100)
+                    _accNo = value;
+            }
+        }
+
+        public int Balance
+        {
+            get
+            {
+                return _accBal;
+            }
+            set
+            {
+                if (value >= 1000)
+                    _accBal = value;
+            }
+        }
+
+        public void deposit(int deposit)
+        {
+            _accBal += deposit;
+        }
+
+        public int withdraw(int withdraw)
+        {
+            if (_accBal - withdraw < 1000)
+                throw new Exception("Insufficient Balance!!!");
+            else
+            {
+                _accBal = _accBal - withdraw;
+                return _accBal;
+            }
+        }
+    }
+}
---
+++ SerialDeserialApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using AccountApp.Model;
+
+namespace SerialDeserialApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Sid", 102, 1500);
+            a1.deposit(20000);
+
+            Console.WriteLine("\nBefore Serialization Acc Details of A1 i.e. (Initial State): ");
+            PrintAccountDetails(a1);
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream st = new FileStream("AccountDetails.txt", FileMode.Create, FileAccess.Write);
+            formatter.Serialize(st,a1);
+            st.Close();
+            
+            a1.withdraw(10000);
+            a1.withdraw(4500);
+            
+            Console.WriteLine("\nBefore Deserialization Acc Details of A1:");
+            PrintAccountDetails(a1);
+            
+            st = new FileStream("AccountDetails.txt", FileMode.Open, FileAccess.Read);
+            a1 = (Account)formatter.Deserialize(st);
+            
+            Console.WriteLine("\nAfter Deserialization Acc Details of A1:");
+            PrintAccountDetails(a1);
+        
+            Console.ReadLine();
+        }
+        static void PrintAccountDetails(Account acc)
+        {
+            Console.WriteLine("Name: "+acc.Name);
+            Console.WriteLine("Id: " + acc.AccNo);
+            Console.WriteLine("Balance: " + acc.Balance);
+        }
+    }
+}
